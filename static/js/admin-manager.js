81482a79f0dcb7498100c982a9764b88a426187c
64070237 <64070237@kmitl.ac.th>
2023-04-12 20:54:30 +0700

    - All Frontend

--- static/js/admin-manager.js
+++ static/js/admin-manager.js
@@ -1,17 +1,14 @@
-
 function addac(x){
     var box = document.getElementsByClassName("box");
     var ac = document.getElementsByClassName("active");
-    for (var i = 0; i < box.length; i++) {
-        if(ac.length > 0){
-            ac[i].classList.remove('active')
-        }
-        box[x].classList.add('active')
+    if(ac.length > 0){;
+        ac[0].classList.remove('active');
     }
+    box[x].classList.add('active');
 }
 function removeac(){
     var ac = document.getElementsByClassName("active");
     for (var i = 0; i < ac.length; i++) {
-        ac[i].classList.remove('active')
+        ac[i].classList.remove('active');
     } 
 }
