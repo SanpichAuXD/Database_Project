5a000ea3a3c8368398edf00439ea3f4deacef2c6
Sanpis Poolsilpe <67864773+zeraph-christ@users.noreply.github.com>
2023-04-13 17:42:24 +0700

    add env

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
-
+const {SQLPASSWORD, SQLDB} = process.env
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'pass1234',
-  database: 'db_project',
+  password: SQLPASSWORD,
+  database: SQLDB,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
